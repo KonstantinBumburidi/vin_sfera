949bba639b55241b4ed1c3108c08c9aee2166239
KonstantinBumburidi <bumburidi.kosta@gmail.com>
2026-01-20 18:26:17 +0500

    robots.txt рабочий

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 
 from django.http import HttpResponse
-from django.template import loader
+from django.conf import settings
+from django.template.loader import render_to_string
 
 def robots_txt(request):
-    template = loader.get_template("robots.txt")
-    return HttpResponse(template.render(), content_type="text/plain")
-    # return HttpResponse("OK", content_type="text/plain")
+    content = render_to_string(
+        "robots.txt",
+        {
+            "debug": settings.DEBUG,
+            "sitemap_url": request.build_absolute_uri("/sitemap.xml"),
+            "host": request.get_host().split(":")[0],
+        },
+    )
+    return HttpResponse(content, content_type="text/plain")
